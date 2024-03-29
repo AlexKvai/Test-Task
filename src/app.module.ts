3d1c74dd5a3fb1f2f5fe835c53bf6ec4f86a7e7c
alex parfenov <alexp-2004@mail.ru>
2024-03-30 02:40:35 +0700

    Добавление модуля Doctor

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { RecordModule } from './record/record.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { UserModule } from './user/user.module';
+import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, RecordModule, ScheduleModule, UserModule],
+  imports: [ConfigModule.forRoot(), AuthModule, RecordModule, ScheduleModule, UserModule, DoctorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
