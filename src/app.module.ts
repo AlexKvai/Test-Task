da2448a9dee63197164c85438ace92a249d793bc
alex parfenov <alexp-2004@mail.ru>
2024-03-30 02:26:33 +0700

    Добавлен ConfigModule и UserModule в app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { RecordModule } from './record/record.module';
 import { ScheduleModule } from './schedule/schedule.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, RecordModule, ScheduleModule],
+  imports: [ConfigModule.forRoot(), AuthModule, RecordModule, ScheduleModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
