26ff886b4edf58356ee9d98ae8e515239895a12e
alex parfenov <alexp-2004@mail.ru>
2024-03-30 02:28:36 +0700

    Добавлено создание пользователя и получение по email и phone, чтобы проверять уникальность

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { UserDto } from './dto/user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getByEmail(email: string) {
+    return this.prisma.patients.findUnique({
+      where: { email },
+    });
+  }
+
+  async getByPhone(phone: string) {
+    return this.prisma.patients.findUnique({
+      where: { phone },
+    });
+  }
+
+  async create(dto: UserDto) {
+    const user = {
+      email: dto.email,
+      name: dto.name,
+      phone: dto.phone,
+      gender: dto.gender,
+    };
+    return this.prisma.patients.create({ data: user });
+  }
+}
