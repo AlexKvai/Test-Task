875a782790220156a2f3800f635385fdf1eeae30
alex parfenov <alexp-2004@mail.ru>
2024-03-30 02:27:32 +0700

    Добавлен сервис регистрации пользователя

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+  async register(dto: RegisterDto) {
+    const oldUserByEmail = await this.userService.getByEmail(dto.email);
+    const oldUserByPhone = await this.userService.getByPhone(dto.phone);
+
+    if (oldUserByEmail)
+      throw new BadRequestException('Пользователь уже существует');
+
+    if (oldUserByPhone)
+      throw new BadRequestException('Пользователь уже существует');
+
+    const user = await this.userService.create(dto);
+
+    return {
+      user,
+    };
+  }
+}
