88e49d5e7721fdc8c7734755c21c64af86ad8f58
alex parfenov <alexp-2004@mail.ru>
2024-03-30 11:45:15 +0700

    Удалён лишний UserModule в ScheduleModule

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DoctorsModule } from 'src/doctors/doctors.module';
 import { PrismaService } from 'src/prisma.service';
-import { UserModule } from 'src/user/user.module';
 import { ScheduleController } from './schedule.controller';
 import { ScheduleService } from './schedule.service';
 
 @Module({
-  imports: [UserModule, DoctorsModule],
+  imports: [DoctorsModule],
   controllers: [ScheduleController],
   providers: [ScheduleService, PrismaService],
 })
