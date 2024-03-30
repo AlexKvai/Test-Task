98b2e4cdb005b2c8500a9f253385b58c9ed228dc
alex parfenov <alexp-2004@mail.ru>
2024-03-30 13:47:35 +0700

    Создан сервис отправки сообщений на mail

---
+++ src/mailer/mailer.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import { EmailDto } from './dto/email.dto';
+
+@Injectable()
+export class MailerService {
+  constructor(private readonly configService: ConfigService) {}
+
+  mailTransport() {
+    const transporter = nodemailer.createTransport({
+      host: this.configService.get<string>('MAIL_HOST'),
+      port: this.configService.get<number>('MAIL_PORT'),
+      secure: false,
+      auth: {
+        user: this.configService.get<string>('MAIL_USER'),
+        pass: this.configService.get<string>('MAIL_PASSWORD'),
+      },
+    });
+    return transporter;
+  }
+
+  async sendEmail(dto: EmailDto) {
+    const { subject, text, to } = dto;
+    const transporter = this.mailTransport();
+
+    const result = await transporter.sendMail({
+      from: '"МИС" <mis@example.com>',
+      subject,
+      text,
+      to,
+    });
+    return result;
+  }
+}
