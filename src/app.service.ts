a120e6da4ee4a55ef0e84316c14ffe5665579821
alex parfenov <alexp-2004@mail.ru>
2024-03-30 15:32:06 +0700

    Поставлена проверка раз в минуту для уведомлений на почту пациента

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { NotificationsService } from './notifications/notifications.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly notificationsService: NotificationsService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron(CronExpression.EVERY_MINUTE)
+  handleCron() {
+    this.notificationsService.sendNotifications();
   }
 }
