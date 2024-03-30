f1e0a329e290b5790e66395870f58671f5f0fd95
alex parfenov <alexp-2004@mail.ru>
2024-03-30 15:31:37 +0700

    Изменение названия ScheduleModule из-за конфликта с библиотекой по уведомлениям

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { RecordModule } from './record/record.module';
-import { ScheduleModule } from './schedule/schedule.module';
-import { UserModule } from './user/user.module';
 import { DoctorsModule } from './doctors/doctors.module';
 import { MailerModule } from './mailer/mailer.module';
+import { RecordModule } from './record/record.module';
+import { ScheduleModule as ScheduleModuleCustom } from './schedule/schedule.module';
+import { UserModule } from './user/user.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, RecordModule, ScheduleModule, UserModule, DoctorsModule, MailerModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    RecordModule,
+    ScheduleModuleCustom,
+    UserModule,
+    DoctorsModule,
+    MailerModule,
+    ScheduleModule.forRoot(),
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
