33c8e6338b8844d870bbd2fd3677a3f8bb9fb411
alex parfenov <alexp-2004@mail.ru>
2024-03-30 13:18:58 +0700

    Добавлено получение расписания в сервисе

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { DoctorsService } from 'src/doctors/doctors.service';
 import { PrismaService } from 'src/prisma.service';
-import { ScheduleDto } from './dto/schedule.dto';
+import { GetScheduleDto, ScheduleDto } from './dto/schedule.dto';
 
 @Injectable()
 export class ScheduleService {
@@ -14,6 +14,31 @@ export class ScheduleService {
     return this.prisma.schedule.findUnique({
       where: { id },
     });
+  }
+
+  async getSchedule(dto: GetScheduleDto) {
+    const { date, doctor_id, patient_id, time_from, time_to, is_free } = dto;
+
+    const dateFrom = new Date(date);
+    const dateTo = new Date(dateFrom);
+    dateTo.setDate(dateFrom.getDate() + 1);
+
+    const paramsBody: any = {
+      time_from: { gte: dateFrom.toISOString() },
+      time_to: { lte: dateTo.toISOString() },
+    };
+
+    if (doctor_id) paramsBody.doctor_id = doctor_id;
+    if (time_from) paramsBody.time_from.gte = new Date(time_from).toISOString();
+    if (time_to) paramsBody.time_to.lte = new Date(time_to).toISOString();
+    if (is_free !== undefined) paramsBody.is_free = is_free;
+    if (patient_id) paramsBody.patient_id = patient_id;
+
+    const schedule = await this.prisma.schedule.findMany({
+      where: paramsBody,
+    });
+
+    return schedule;
   }
 
   async create(dto: ScheduleDto) {
